1531b94673aca17bdd5f959be19114ee182974ba
Ryo Vamirion <rynortheast@yandex.ru>
2023-08-02 16:13:28 +0300

    Модифицирован .bat для GBEtJ: Добавлена возможность открыть конфигуратор

--- launch/generator-blocks-excel-to-json.bat
+++ launch/generator-blocks-excel-to-json.bat
@@ -1,8 +1,22 @@
 
 @echo off
 
-if not exist "./../node_modules" (
-    cmd /c cd ./.. & npm i & cls & node ./../src/generator-blocks-excel-to-json/index.js & pause
+set /p answer="Open Config or Run Program [1 / anything]: "
+
+if %answer% EQU 1 (
+
+    start ./../config/generator-blocks-excel-to-json.json
+
 ) else (
-    cmd /c node ./../src/generator-blocks-excel-to-json/index.js & pause
+
+    if not exist "./../node_modules" (
+
+        cmd /c cd ./.. & npm i & cls & node ./../src/generator-blocks-excel-to-json/index.js & pause
+
+    ) else (
+
+        cmd /c node ./../src/generator-blocks-excel-to-json/index.js & pause
+
+    )
+
 )
