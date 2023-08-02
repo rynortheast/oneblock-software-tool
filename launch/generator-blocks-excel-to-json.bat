4ba5be16020afc8c77999babbccaa8adb794e4b5
Ryo Vamirion <rynortheast@yandex.ru>
2023-08-02 15:53:58 +0300

    Модифицирован .bat для GBEtJ: Добавлено отслеживание и установка node_modules

--- launch/generator-blocks-excel-to-json.bat
+++ launch/generator-blocks-excel-to-json.bat
@@ -1,4 +1,8 @@
 
-node ./../src/generator-blocks-excel-to-json/index.js
+@echo off
 
-pause
+if not exist "./../node_modules" (
+    cmd /c cd ./.. & npm i & cls & node ./../src/generator-blocks-excel-to-json/index.js & pause
+) else (
+    cmd /c node ./../src/generator-blocks-excel-to-json/index.js & pause
+)
